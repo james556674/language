e06ad26f4ea856ef72a464ceee14195b8a2ad0f0
James <james@sanuker.com>
2024-06-01 14:40:48 +0800

    feat: install fontawesome

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 
 export default function Root() {
   return (
     <>
       {/* all the other elements */}
+      <div className="header">
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      
       <div id="detail">
         <Outlet />
       </div>
